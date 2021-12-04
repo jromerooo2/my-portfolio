f7a81acba73c33d6a40e321bd48a75f34853a2c2
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-12-03 21:33:45 -0600

    :cupcake: cool colors on phone

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,7 +8,7 @@
         </div>   
         <div class="flex items-center justify-center ">
             <div class="">
-                <a @click="redirect(3)" class=" flex items-center text-3xl font-bold cursor-pointer hover:text-blue-500">
+                <a @click="redirect(3)" class=" flex items-center text-3xl font-bold cursor-pointer text-blue-500 md:text-black md:hover:text-blue-500">
                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="104" height="104" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <rect x="4" y="4" width="16" height="16" rx="2" />
@@ -19,21 +19,21 @@
                     </svg>
                     <span>LinkedIn</span>
                 </a>
-                <a @click="redirect(1)" class=" flex items-center justify-center text-3xl font-bold cursor-pointer text-black hover:text-purple-500 ">
+                <a @click="redirect(1)" class=" flex items-center justify-center text-3xl font-bold cursor-pointer text-black hover:text-purple-500 text-purple-500 md:text-black">
                     <span>Github</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="104" height="104" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                     </svg>
                 </a>
-                <a @click="redirect(2)" class=" flex items-center text-3xl font-bold cursor-pointer hover:text-blue-500">
+                <a @click="redirect(2)" class=" flex items-center text-3xl font-bold cursor-pointer md:hover:text-blue-500  text-blue-500 md:text-black">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="104" height="104" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
                         </svg>
                     <span>Twitter</span>
                 </a>
-                <a @click="redirect(4)" class=" flex items-center text-3xl font-bold cursor-pointer hover:text-yellow-500">
+                <a @click="redirect(4)" class=" flex items-center text-3xl font-bold cursor-pointer  text-yellow-500 md:text-black md:hover:text-yellow-500">
                     <span>E-mail</span>
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-at" width="104" height="104" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>

b572a14621a84fb043cb0003ead3f1cf47979b2a
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-04-29 08:36:37 -0600

    changes

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,9 +1,11 @@
 <template>
         <div class="container-flow flex md:block md:overflow-hidden overflow-scroll mb-20 md:mb-0">
             <div class="md:h-screen flex flex-col items-center justify-center p-4 md:p-9 min-w-full" v-for="work in works" :key="work.id">
-                <h1 :class="work.color" >{{work.title}}</h1>
-                <img class="my-5"  :src="work.image"/>
-                <p class="text-justify text-xl">{{work.description}}</p>
+                <a :href="work.link !== '' ? work.link :'javascript:void(0);'" :class="{'cursor-default':!hasLink}">
+                    <h1 :class="work.color" >{{work.title}}</h1>
+                    <img class="my-5"  :src="work.image"/>
+                    <p class="text-justify text-xl">{{work.description}}</p>
+                </a>
             </div>
         </div>
 </template>
@@ -19,6 +21,11 @@ export default defineComponent({
             type: Array as PropType<Work[]>
         },
     },
+    data(){
+        return{
+            hasLink:true,
+        }
+    }
 })
 </script>
 

5913a193ed186afa2f014c017a5c75022dc84517
jromerooo2 <juan.romeroramos9@gmail.com>
2024-07-10 17:50:48 -0500

    Update resume link in Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -143,7 +143,7 @@ export default defineComponent({
             </RouterLink>
           </li>
           <li class="text-sm text-gray-800 font-bold hover:text-blue-200">
-            <a class="flex items-center space-x-2" target="_blank" href="https://raw.githubusercontent.com/jromerooo2/my-portfolio/dev/src/assets/resume.pdf">
+            <a class="flex items-center space-x-2" target="_blank" href="/public/resume.pdf">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-download" width="34" height="34" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
